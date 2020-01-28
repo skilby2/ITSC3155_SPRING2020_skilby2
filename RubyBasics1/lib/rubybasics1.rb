d8912adc61227eab2f6965ffc2bf6ef84583716a
Samuel Kilby <skilby2@uncc.edu>
2020-01-28 18:59:24 +0000

    RubyBasics - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,16 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  arr.sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
