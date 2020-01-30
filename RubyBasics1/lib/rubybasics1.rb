d180475e0a5e80a2217fe75e3f44bad12b12a2e3
Samuel Kilby <skilby2@uncc.edu>
2020-01-30 15:41:49 +0000

    RubyBasics1 - Part 2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -8,6 +8,29 @@ end
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  if (arr.length() == 0)
+    return 0
+  elsif (arr.length() == 1)
+    return arr[0];
+  else
+    num1 = 0
+    num2 = -9999999
+    not2 = 0
+    arr.sort
+    for i in (0...arr.length())
+      if arr[i] > num1
+        num1 = arr[i]
+        not2 = i
+      end
+    end
+    arr.delete_at(not2)  
+    for i in (0...arr.length())
+      if arr[i] > num2
+        num2 = arr[i]
+      end
+    end
+    return num1 + num2
+  end
 end
 
 # Part III
