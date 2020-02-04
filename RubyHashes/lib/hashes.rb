7085d279b5ff293f4163fe02ddbc26bb978455b5
Samuel Kilby <skilby2@uncc.edu>
2020-02-04 16:58:58 +0000

    RubyHashes - Part 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,6 +2,14 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    x = 0
+    contacts.each do |key, value|
+        contacts[key] = emails[x]
+        if emails[x] == nil
+            contacts[key] = ""
+        end
+        x+=1
+    end
 end
 
 # Part II
