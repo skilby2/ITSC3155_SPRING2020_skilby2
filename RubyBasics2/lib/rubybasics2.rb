eef02c7697dccc403d1e7272930b91c0da044a6d
Samuel Kilby <skilby2@uncc.edu>
2020-02-06 03:35:09 +0000

    Ruby Basics 2 - Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -10,9 +10,6 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
-  # \W not a word character
-  # /begin expression /close expression
-  # ^ not
   if /^[^aeiou\W]/i.match(s) == nil
     return false
   else
@@ -23,4 +20,43 @@ end
 # Part III
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s == "0"
+    return true
+  end
+  if /^[10]*00$/.match(s) != nil
+    return true
+  else
+    return false
+  end
+  
 end
+
+# [abc]	  A single character of: a, b, or c
+# [^abc]	Any single character except: a, b, or c
+# [a-z]	  Any single character in the range a-z
+# [a-zA-Z]Any single character in the range a-z or A-Z
+# ^	      Start of line
+# $	      End of line
+# \A     	Start of string
+# \z	    End of string
+# .	      Any single character
+# \s	    Any whitespace character
+# \S	    Any non-whitespace character
+# \d	    Any digit
+# \D	    Any non-digit
+# \w	    Any word character (letter, number, underscore)
+# \W	    Any non-word character
+# \b	    Any word boundary
+# (...)	  Capture everything enclosed
+# (a|b)	  a or b
+# a?	    Zero or one of a
+# a*	    Zero or more of a
+# a+	    One or more of a
+# a{3}	  Exactly 3 of a
+# a{3,}	  3 or more of a
+# a{3,6}	Between 3 and 6 of a
+
+# i   case insensitive 
+# m   make dot match newlines 
+# x   ignore whitespace in regex 
+# o   perform #{...} substitutions only once
