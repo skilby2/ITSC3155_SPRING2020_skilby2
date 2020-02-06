d8e6b11d1afe07d303a24c1154b07a3b8ac821ca
Samuel Kilby <skilby2@uncc.edu>
2020-02-06 02:31:56 +0000

    Ruby Basics 2 - Part 2

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -3,11 +3,21 @@
 # Part I
 def hello(name)
   # YOUR CODE HERE
+  #return a value in methods
+  return "Hello, " + name
 end
 
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+  # \W not a word character
+  # /begin expression /close expression
+  # ^ not
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end # end of if/else statement
 end
 
 # Part III
