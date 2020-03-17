b6d1d248233aaffb4add93a291f235319d578dc0
Samuel Kilby <skilby2@uncc.edu>
2020-03-17 02:09:06 +0000

    Complete Create and Show.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,8 +1,19 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params[:article])
+        
+        @article.save
+        redirect_to @article
     end
+end
+
+private
+def article_params
+    params.require(:artcile).permit(:title, :text)
 end
